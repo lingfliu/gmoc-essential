d5ea816e5e02be8420c0d69eba198a9e7ccb4e53
lingfliu <lingfeng.liu@163.com>
2022-09-26 13:21:19 +0800

    updated

--- sig_aquisition/bvh/py/bvh_aquisition.py
+++ sig_aquisition/bvh/py/bvh_aquisition.py
@@ -2,38 +2,78 @@ import os
 import re
 
 
-def parse_joint(f):
-    return ''
+def parse_node(f, bracelets):
+    hierarchy = {}
+    hierarchy['children'] = []
+
+    line = f.readline()
+    if not re.search('{', line.strip()):
+        return {}
+    bracelets.append('{')
+
+    line = f.readline()
+    if not re.search('OFFSET', line.strip()):
+        return {}
+    hierarchy['offset'] = line.strip().split(' ')[1:]
+
+    line = f.readline()
+    if not re.search('CHANNELS', line):
+        return {}
+    hierarchy['channels'] = line.strip().split(' ')[2:]
+
+    line = f.readline()
+    while not re.search('}', line) and line:
+        if re.search('JOINT', line):
+            (child_hierarchy, bracelets) = parse_node(f, bracelets)
+            child_hierarchy['category'] = 'joint'
+            child_hierarchy['name'] = line.strip().split(' ')[1]
+            hierarchy['children'].append(child_hierarchy)
+        elif re.search('End Site', line.strip()):
+            end_hierarchy = {}
+            end_hierarchy['category'] = 'end'
+            line = f.readline()
+            line = f.readline()
+            end_hierarchy['offset'] = line.strip().split(' ')[1:]
+            line = f.readline()
+            hierarchy['children'].append(end_hierarchy)
+        line = f.readline()
+
+    return (hierarchy, bracelets)
 
 def bvh_import(path, name):
-    hierarchy = {
-            'category':'ROOT',
-            'name':'hips',
-            'offset': [0,0,0],
-            'channels': ['xpos', 'ypos', 'zpos', 'xrot', 'yrot', 'zrot'],
-            'children': [
-                {'name':'RightUpLeg',}
-            ]
-    }
+    hierarchy = {}
+    bracelets = []
+
 
     with open(os.path.join(path, name)) as f:
+        # locate hierarchy
+        foundHierarchy = False
         line = f.readline()
-        if not re.match('^HIERARCHY', line):
+        while line:
+            if re.match('^HIERARCHY', line.strip()):
+                foundHierarchy = True
+                break
+            line = f.readline()
+        if not foundHierarchy:
             return {}
 
-        # parse hierachy header
         line = f.readline()
-        if not re.match('^ROOT ', line):
+        foundRoot = False
+        while line:
+            if re.match('^ROOT ', line.strip()):
+                foundRoot = True
+                break
+        if not foundRoot:
             return {}
 
-        root_name = line.split(' ')[1].strip()
-        hierarchy['name'] = root_name
+        # parse root
         hierarchy['category'] = 'root'
+        hierarchy['name'] = line.strip().split(' ')[1]
+        hierarchy['children']  = []
 
         line = f.readline()
-        if not re.search('{', line):
+        if not re.search('{', line.strip()):
             return {}
-
         line = f.readline()
         if not re.search('OFFSET', line):
             return {}
@@ -45,16 +85,12 @@ def bvh_import(path, name):
         hierarchy['channels'] = line.strip().split(' ')[2:]
 
         line = f.readline()
-        if re.search('JOINT'):
-            sub_hierachy = parse_joint(f, line.split(' ')[1])
-            hierarchy['children'].append(sub_hierachy)
-
-            # while line:
-            #     # parse hierachy
-            #     # parse header
-            #     line.freadline
-            # else
-            # line = f.readline()
-
+        while line:
+            # recursive reading
+            if re.search('JOINT', line):
+                (child_hierarchy, bracelets) = parse_node(f, bracelets)
+                child_hierarchy['category'] = 'joint'
+                child_hierarchy['name'] = line.strip().split(' ')[1]
+                hierarchy['children'].append(child_hierarchy)
 
     return hierarchy
