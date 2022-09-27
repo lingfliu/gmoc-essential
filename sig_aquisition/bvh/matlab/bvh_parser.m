a361f08515a3c2a6f626e4952d008d936c199862
lingfliu <lingfeng.liu@163.com>
2022-09-27 23:43:39 +0800

    created matlab parser

--- sig_aquisition/bvh/matlab/bvh_parser.m
+++ sig_aquisition/bvh/matlab/bvh_parser.m
@@ -1,6 +1,5 @@
 function [meta, data] = bvh_parser(file)
 %BVH_PARSER bvh data file loader & reader
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+fid = fopen(file);
 end
 
---
+++ sig_aquisition/bvh/matlab/parse_node_attr.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = parse_node_(inputArg1,inputArg2)
+%PARSE_NODE_ 此处显示有关此函数的摘要
+%   此处显示详细说明
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
